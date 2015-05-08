a0af66696c572a62db89fef991b1d56831997811
SergeyFilippov <sergey.filippov@outlook.com>
2015-05-09 00:23:55 +0200

    Idt manager added. Fails compilation - missing reference to DTF WindowsInstaller

---
+++ MsiToSqLiteConverter/MsiToSqLiteConverter.MsiProcessing/Domain/IIdtManager.cs
@@ -0,0 +1,19 @@
+﻿namespace MsiToSqLiteConverter.MsiProcessing.Domain
+{
+    using System;
+
+    public interface IIdtManager : IDisposable
+    {
+        void Connect(string msiPath);
+
+        void Disconnect(bool isDispose);
+
+        void ExtactTable(string tableName, string filePath);
+
+        void ImportTable(string filePath);
+
+        void ExportAll(string directoryPath);
+
+        void ImportAll(string directoryPath);
+    }
+}
