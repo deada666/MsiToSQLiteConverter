d3a874071b9ed1b909279072f400403d2a5cfb6b
fenriv <fenriv@gmail.com>
2015-05-09 16:37:34 +0200

    #1 missing files added

---
+++ MsiToSqLiteConverter/MsiToSqLiteConverter.MsiProcessing/Domain/IdtReader.cs
@@ -0,0 +1,69 @@
+﻿namespace MsiToSqLiteConverter.MsiProcessing.Domain
+{
+    using System;
+    using System.IO;
+
+    using MsiToSqLiteConverter.MsiProcessing.Exceptions;
+
+    /// <summary>
+    /// The IDT reader.
+    /// </summary>
+    public class IdtReader
+    {
+        /// <summary>
+        /// Reads the table schema.
+        /// </summary>
+        /// <param name="idtFilePath">
+        /// The IDT file path.
+        /// </param>
+        /// <returns>
+        /// MSI table schema.
+        /// </returns>
+        /// <exception cref="System.ArgumentException">
+        /// IDT file not found. Schema cannot be extracted.
+        /// </exception>
+        public MsiTableSchema ReadTableSchema(string idtFilePath)
+        {
+            if (!File.Exists(idtFilePath))
+            {
+                throw new ArgumentException("Idt file not found. Schema cannot be extracted.");
+            }
+
+            string columnNames;
+            string columnDefinitions;
+            string tableProperties;
+
+            using (var fileReader = new FileStream(idtFilePath, FileMode.Open))
+            {
+                using (var reader = new StreamReader(fileReader))
+                {
+                    try
+                    {
+                        columnNames = reader.ReadLine();
+                        columnDefinitions = reader.ReadLine();
+                        tableProperties = reader.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(columnNames) || string.IsNullOrWhiteSpace(columnDefinitions)
+                            || string.IsNullOrWhiteSpace(tableProperties))
+                        {
+                            throw new IdtParseException("IdtFile is corrupted. Schema cannot be extracted.");
+                        }
+                    }
+                    catch (IdtParseException)
+                    {
+                        throw;
+                    }
+                    catch (Exception exception)
+                    {
+                        throw new IdtParseException("Error processing IDT file. Schema cannot be extracted.", exception);
+                    }
+                }
+            }
+
+            var result = new MsiTableSchema(columnNames, columnDefinitions, tableProperties);
+            return result;
+        }
+
+
+    }
+}
---
+++ MsiToSqLiteConverter/MsiToSqLiteConverter.Tests/IdtReaderTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsiToSqLiteConverter.Tests
+{
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    using MsiToSqLiteConverter.MsiProcessing.Domain;
+
+    [TestClass]
+    public class IdtReaderTest
+    {
+        /// <summary>
+        /// Reads the IDT.
+        /// </summary>
+        [TestMethod]
+        [DeploymentItem("Resources\\ControlEvent.idt")]
+        [DeploymentItem("Resources\\Component_1251.idt")]
+        [DeploymentItem("Resources\\Cabs.idt")]
+        public void ReadIdt()
+        {
+            var reader = new IdtReader();
+
+            var cabSchema = reader.ReadTableSchema("Cabs.idt");
+            var componentSchema = reader.ReadTableSchema("Component_1251.idt");
+            var controlEventSchema = reader.ReadTableSchema("ControlEvent.idt");
+
+            Assert.AreEqual(cabSchema.TableName, "Cabs", false);
+            Assert.AreEqual(componentSchema.TableName, "Component", false);
+            Assert.AreEqual(controlEventSchema.TableName, "ControlEvent", false);
+
+            Assert.AreEqual(cabSchema.Columns.Count, 2);
+            Assert.AreEqual(componentSchema.Columns.Count, 6);
+            Assert.AreEqual(controlEventSchema.Columns.Count, 6);
+
+            Assert.AreEqual(cabSchema.Columns.Where(item => item.Value.IsKeyColumn).ToList().Count, 1);
+            Assert.AreEqual(componentSchema.Columns.Where(item => item.Value.IsKeyColumn).ToList().Count, 1);
+            Assert.AreEqual(controlEventSchema.Columns.Where(item => item.Value.IsKeyColumn).ToList().Count, 5);
+        }
+    }
+}
